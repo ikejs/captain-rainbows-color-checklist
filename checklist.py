e29078f0ffb56799d383fc4d0e14f4f52a57b50e
Ike <ikeholzmann@gmail.com>
2019-09-03 10:38:29 -0700

    [add] select prompt/options, reword, comment

--- checklist.py
+++ checklist.py
@@ -6,41 +6,74 @@ def create(item):
 
 # READ
 def read(index):
-    item = checklist[index]
-    return item
+    item = checklist[int(index)]
+    print(item)
 
 # UPDATE
-def update(index, item):
-    checklist[index] = item
+def update(index, newName):
+    checklist[int(index)] = newName
 
 # DESTROY
 def destroy(index):
-    checklist.pop(index)
+    checklist.pop(int(index))
 
 # LIST ALL ITEMS
 def list_all_items():
     index = 0
     for list_item in checklist:
-        print("{} {}".format(index, list_item))
+        print("{} {}".format(int(index), list_item))
         index += 1
 
 # MARK COMPLETED
 def mark_completed(index):
     checklist[index] = "√ " + checklist[index]
 
-def test():
-    create("purple sox")
-    create("red cloak")
+# USER INPUT
+def user_input(prompt):
+    user_input = input(prompt)
+    return user_input
 
-    print(read(0))
-    print(read(1))
+# SELECT
+def select(function_code):
+    # Ignore Case
+    function_code = function_code.upper()
 
-    update(0, "purple socks")
+    # Create item
+    if function_code == "C":
+        input_item = user_input("Input item: ")
+        create(input_item)
 
-    destroy(1)
+    # Read item
+    elif function_code == "R":
+        item_index = user_input("Index Number: ")
+        read(item_index)
 
-    print(read(0))
+    # Update item
+    elif function_code == "U":
+        item_index = user_input("Index Number to Update: ")
+        changeTo = user_input("New Item Name: ")
+        update(item_index, changeTo)
 
-    list_all_items()
+    # Destroy item
+    elif function_code == "D":
+        item_index = user_input("Index Number to Delete: ")
+        destroy(item_index)
 
-test()
+    # Print all items
+    elif function_code == "P":
+        list_all_items()
+
+    # Quit
+    elif function_code == "Q":
+        return False
+
+    # Catch all
+    else:
+        print("Unknown Option")
+    return True
+
+running = True
+while running:
+    print("Press C to add to list, R to Read from list, P to display list, U to update an item, D to delete an item, and Q to quit")
+    selection = user_input("")
+    running = select(selection)
